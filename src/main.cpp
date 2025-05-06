dd1d04c0285a945d028f50b2c89f1f94c7580c27
PortalPC <ian.portal@usil.pe>
2025-05-05 19:03:59 -0500

    Actualización_Portal_1

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,42 @@
-#include <Arduino.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#define F_CPU 16000000UL
 
-// put function declarations here:
-int myFunction(int, int);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+char cuenta_binario = 0;
+char cuenta_gray = 0;
+ char a =0;
+  void config_IE (void){
+    EIMSK |= (1<<INT0); // Habilitar interrupción externa INT0
+    EICRA |= (1<<ISC01); // Configurar INT0 para disparar en flanco de bajada
+    DDRD &= ~(0X04);
+    PORTD |= 0x04;
+
+  }
+
+  ISR(INT0_vect) {
+    a++;
+    if(a>1){
+      a = 0;
+    }
+    
+  }
+  int main(void){
+DDRD |= 0x0F;
+DDRB |= 0x0F;
+while(1){
+  cuenta_binario++;
+
+  if(cuenta_binario >15){
+    cuenta_binario = 0;
+  }
+  
+cuenta_gray = cuenta_binario ^ (cuenta_binario << 1); // Convertir a Gray
+if(a ==1){
+  PORTD |=((cuenta_binario | 0x00) << 4);
+
 }
-
-void loop() {
-  // put your main code here, to run repeatedly:
 }
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+  }
+  
